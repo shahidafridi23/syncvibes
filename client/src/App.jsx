4a779c8e6fc01b43c695e33452e17cf1ad2c243b
Shahid Afridi <shafridi9807@gmail.com>
2024-11-17 17:45:08 +0530

    Client-Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-semibold">
+      <Button>jhghg</Button>
+    </div>
+  );
+};
+
+export default App;
