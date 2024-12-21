372841a32373928b1370ba4091552c1cead97d30
Shahid Afridi <shafridi9807@gmail.com>
2024-12-21 21:48:29 +0530

    Added axios inceptor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,17 @@ import App from "./App.jsx";
 import axios from "axios";
 
 axios.defaults.baseURL = `${import.meta.env.VITE_SERVER_URL}`;
-axios.defaults.withCredentials = true;
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 createRoot(document.getElementById("root")).render(<App />);
